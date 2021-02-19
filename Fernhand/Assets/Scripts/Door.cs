18ca46989a09d33426dbf8c24d98ebd147d85d1e
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2021-02-19 20:27:54 +0100

    initial commit

---
+++ Fernhand/Assets/Scripts/Door.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Animator _animator;
+    private static readonly int Open = Animator.StringToHash("Open");
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            _animator.SetBool(Open, true);
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            _animator.SetBool(Open, false);
+    }
+}
